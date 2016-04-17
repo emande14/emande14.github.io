9f36cc889ca221bd62f6d4b9b481f76017f40056
emande14 <emande14@smumn.edu>
2016-04-16 22:58:34 -0500

    Barebones
    This should work. Put the js w/in the html rather than it's own js file.

--- js/map.js
+++ js/map.js
@@ -4,6 +4,6 @@ var mymap = L.map('mapid').setView([44.9537, -93.0900], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'emande14.fe985a38'
+    id: 'emande14.fe985a38',
     accessToken: 'pk.eyJ1IjoiZW1hbmRlMTQiLCJhIjoiM3pjYXRxNCJ9.rrDLlZxSn5ivm_VqPwf0uw'
 }).addTo(mymap);
